b2ad0a86deecc82edee7e95b40a0e092e16b97ac
SajjadAli555 <76934036+SajjadAli555@users.noreply.github.com>
2024-10-02 11:04:49 +0500

    Update router.js

--- api/Router/router.js
+++ api/Router/router.js
@@ -14,7 +14,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const dir = path.join(__dirname, "../public/images");
+    const dir = path.join('/tmp', 'images');
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir, { recursive: true });
     }
